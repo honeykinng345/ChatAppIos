1dc8e3f569bad0f836d8e6c537febb56e5b9e55c
rehan <rehanmunir217@gmail.com>
2024-01-08 17:21:26 +0400

    added navigation screen and added firebase pod implement register and login

---
+++ Flash Chat iOS13/utilis/HelperFunctions.swift
@@ -0,0 +1,9 @@
+//
+//  HelperFunctions.swift
+//  Flash Chat iOS13
+//
+//  Created by Rehan on 05/01/2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
---
+++ Flash Chat iOS13/utilis/screenRoutes.swift
@@ -0,0 +1,9 @@
+//
+//  screenRoutes.swift
+//  Flash Chat iOS13
+//
+//  Created by Rehan on 05/01/2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
